27a5ec60c8e7c94d740980344bcc8afb33695bba
hooni <jhlee909090@gmail.com>
2024-11-13 16:20:48 +0900

    [Add] new Xcode project configuration

---
+++ saftyReport/saftyReport/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  saftyReport
+//
+//  Created by 이지훈 on 11/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
