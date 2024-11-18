0518aa0b3cf5275de632c10c22bfedb4011f5c87
hooni <jhlee909090@gmail.com>
2024-11-19 01:15:31 +0900

    style/#12 - 각 셀 분리

---
+++ saftyReport/saftyReport/Source/ReprotDetail/Cell/BaseCell.swift
@@ -0,0 +1,49 @@
+//
+//  BaseCell.swift
+//  Solo
+//
+//  Created by 이지훈 on 11/17/24.
+//
+
+import UIKit
+
+class BaseCell: UICollectionViewCell {
+    static var reuseIdentifier: String { return String(describing: self) }
+    
+    let titleLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 16, weight: .medium)
+    }
+    
+    let requiredMark = UILabel().then {
+        $0.text = "*"
+        $0.textColor = .red
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupBaseUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupBaseUI() {
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(requiredMark)
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.leading.equalToSuperview()
+        }
+        
+        requiredMark.snp.makeConstraints {
+            $0.leading.equalTo(titleLabel.snp.trailing).offset(4)
+            $0.centerY.equalTo(titleLabel)
+        }
+    }
+    
+    func configure(with item: ReportViewController.Item) {
+        titleLabel.text = item.title
+        requiredMark.isHidden = !item.isRequired
+    }
+}
