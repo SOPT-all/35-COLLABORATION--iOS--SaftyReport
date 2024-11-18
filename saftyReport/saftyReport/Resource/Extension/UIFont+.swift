82ce60136fc682a5c4cea27f4da0b4729ffed8b7
Yurim Kim <yurim830@gmail.com>
2024-11-18 21:56:57 +0900

    Add, Feat/#5 - UIFont+ 파일 생성 및 pretendard 메소드 구현

--- saftyReport/saftyReport/Resource/Extension/UIFont+.swift
+++ saftyReport/saftyReport/Resource/Extension/UIFont+.swift
@@ -9,4 +9,24 @@ import UIKit
 
 extension UIFont {
     
+    enum PretendardWeight: String {
+        case black = "Pretendard-Black"
+        case bold = "Pretendard-Bold"
+        case extraBold = "Pretendard-ExtraBold"
+        case extraLight = "Pretendard-ExtraLight"
+        case light = "Pretendard-Light"
+        case medium = "Pretendard-Medium"
+        case regular = "Pretendard-Regular"
+        case semibold = "Pretendard-SemiBold"
+        case thin = "Pretendard-Thin"
+    }
+    
+    static func pretendard(weight: PretendardWeight, size: CGFloat) -> UIFont {
+        guard let font = UIFont(name: weight.rawValue, size: size) else {
+            print("🤥 Pretendard 폰트 변환 실패")
+            return .systemFont(ofSize: size)
+        }
+        
+        return font
+    }
 }
