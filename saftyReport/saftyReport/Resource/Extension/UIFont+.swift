9b7e506c0bb306ebd3222ae7122c253402ebf464
hooni <jhlee909090@gmail.com>
2024-11-19 17:02:25 +0900

    fix/#14- 코드삭제

--- saftyReport/saftyReport/Resource/Extension/UIFont+.swift
+++ saftyReport/saftyReport/Resource/Extension/UIFont+.swift
@@ -28,45 +28,5 @@ extension UIFont {
         
         return font
     }
-      static var heading01: UIFont { .pretendard(weight: .semibold, size: 20) }
-      static var heading02: UIFont { .pretendard(weight: .bold, size: 18) }
-      
-      static var body01: UIFont { .pretendard(weight: .extraBold, size: 16) }
-      static var body02: UIFont { .pretendard(weight: .bold, size: 16) }
-      static var body03: UIFont { .pretendard(weight: .semibold, size: 16) }
-      static var body04: UIFont { .pretendard(weight: .bold, size: 15) }
-      static var body05: UIFont { .pretendard(weight: .regular, size: 15) }
-      static var body06: UIFont { .pretendard(weight: .bold, size: 14) }
-      static var body07: UIFont { .pretendard(weight: .bold, size: 14) }
-      static var body08: UIFont { .pretendard(weight: .medium, size: 14) }
-      static var body09: UIFont { .pretendard(weight: .medium, size: 14) }
-      
-      static var caption01: UIFont { .pretendard(weight: .bold, size: 12) }
-      static var caption02: UIFont { .pretendard(weight: .semibold, size: 12) }
-      static var caption03: UIFont { .pretendard(weight: .medium, size: 12) }
-      static var caption04: UIFont { .pretendard(weight: .regular, size: 12) }
-      static var caption05: UIFont { .pretendard(weight: .bold, size: 10) }
-      static var caption06: UIFont { .pretendard(weight: .semibold, size: 10) }
-      static var caption07: UIFont { .pretendard(weight: .medium, size: 10) }
-      static var caption08: UIFont { .pretendard(weight: .bold, size: 8) }
-      static var caption09: UIFont { .pretendard(weight: .medium, size: 8) }
     
-    var lineHeight: CGFloat {
-        get {
-            switch self {
-            case UIFont.heading01: return 24
-            case UIFont.heading02: return 21
-            case UIFont.body01, UIFont.body02: return 20
-            case UIFont.body03: return 24
-            case UIFont.body04, UIFont.body05: return 18
-            case UIFont.body06, UIFont.body08: return 17
-            case UIFont.body07, UIFont.body09: return 20
-            case UIFont.caption01, UIFont.caption02, UIFont.caption03: return 14
-            case UIFont.caption04: return 19
-            case UIFont.caption05, UIFont.caption06, UIFont.caption07: return 12
-            case UIFont.caption08, UIFont.caption09: return 10
-            default: return self.pointSize * 1.2 // 기본 line height
-            }
-        }
-    }
 }
