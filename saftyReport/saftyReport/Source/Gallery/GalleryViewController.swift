e7ce55d617370fd46f255f661adcda2639701828
OneTen19 <khy000426@naver.com>
2024-11-19 23:51:39 +0900

    feat/#10 - GalleryViewController Compositional Layout

--- saftyReport/saftyReport/Source/Gallery/GalleryViewController.swift
+++ saftyReport/saftyReport/Source/Gallery/GalleryViewController.swift
@@ -13,28 +13,165 @@ import Kingfisher
 
 class GalleryViewController: UIViewController {
     
-    private var baseView: UICollectionView!
-    private var datasource: UICollectionViewDiffableDataSource<GallerySection, GalleryItem>!
+    private let collectionView = UICollectionView(frame: .zero,
+                                                  collectionViewLayout: UICollectionViewLayout())
+    private var dataSource: UICollectionViewDiffableDataSource<GallerySectionType, GalleryItem>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setUI()
         setLayout()
+        setupCollectionView()
     }
     
     private func setUI() {
-        view.addSubview(baseView)
+        self.view.addSubview(collectionView)
     }
     
     private func setLayout() {
-        baseView.snp.makeConstraints {
+        collectionView.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
     }
     
+    private func setupCollectionView() {
+        collectionView.collectionViewLayout = createLayout()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        collectionView.register(MediaSelectCell.self,
+                                forCellWithReuseIdentifier: MediaSelectCell.cellIdentifier)
+        collectionView.register(WarningCell.self,
+                                forCellWithReuseIdentifier: WarningCell.cellIdentifier)
+        collectionView.register(ContentsCell.self,
+                                forCellWithReuseIdentifier: ContentsCell.cellIdentifier)
+    }
+    
+    private func createLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout { [weak self] sectionIndex, _ in
+            guard let sectionType = GallerySectionType(rawValue: sectionIndex) else { return nil }
+            
+            switch sectionType {
+            case .mediaSelect:
+                return self?.createMediaSelectSection()
+            case .warning:
+                return self?.createWarningSection()
+            case .contents:
+                return self?.createContentsSection()
+            }
+        }
+        return layout
+    }
+
+    
+    private func createMediaSelectSection() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1),
+            heightDimension: .fractionalHeight(1)
+        )
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1),
+            heightDimension: .absolute(40)
+        )
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: groupSize,
+            subitems: [item]
+        )
+        
+        let section = NSCollectionLayoutSection(group: group)
+        
+        return section
+    }
+    
+    private func createWarningSection() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1),
+            heightDimension: .fractionalHeight(1)
+        )
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1),
+            heightDimension: .absolute(38)
+        )
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: groupSize,
+            subitems: [item]
+        )
+        
+        return NSCollectionLayoutSection(group: group)
+    }
+    
+    private func createContentsSection() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1/3),
+            heightDimension: .fractionalHeight(1/2)
+        )
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1),
+            heightDimension: .absolute(253)
+        )
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: groupSize,
+            subitems: [item]
+        )
+        
+        return NSCollectionLayoutSection(group: group)
+    }
     
     
     
 }
 
-//#Preview { GalleryViewController() }
+extension GalleryViewController : UICollectionViewDelegate{
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return 1
+        case 1:
+            return 1
+        case 2:
+            return 6
+        default:
+            return 0
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        switch indexPath.section {
+        case 0:
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MediaSelectCell.cellIdentifier, for: indexPath) as? MediaSelectCell else {
+                return UICollectionViewCell(frame: .zero)
+            }
+            return cell
+        case 1:
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MediaSelectCell.cellIdentifier, for: indexPath) as? MediaSelectCell else {
+                return UICollectionViewCell(frame: .zero)
+            }
+            return cell
+        case 2:
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MediaSelectCell.cellIdentifier, for: indexPath) as? MediaSelectCell else {
+                return UICollectionViewCell(frame: .zero)
+            }
+            return cell
+        default:
+            return UICollectionViewCell()
+            
+        }
+        
+    }
+}
+
+extension GalleryViewController: UICollectionViewDataSource{
+    
+}
