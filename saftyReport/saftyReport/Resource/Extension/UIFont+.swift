e3c4873bdbee5e1a12c7bf151c87ed6c741d9d2f
hooni <jhlee909090@gmail.com>
2024-11-19 10:08:20 +0900

    add/#12 타이포그래피 추가

--- saftyReport/saftyReport/Resource/Extension/UIFont+.swift
+++ saftyReport/saftyReport/Resource/Extension/UIFont+.swift
@@ -28,4 +28,26 @@ extension UIFont {
         
         return font
     }
+      static var heading01: UIFont { .pretendard(weight: .semibold, size: 20) }
+      static var heading02: UIFont { .pretendard(weight: .bold, size: 18) }
+      
+      static var body01: UIFont { .pretendard(weight: .extraBold, size: 16) }
+      static var body02: UIFont { .pretendard(weight: .bold, size: 16) }
+      static var body03: UIFont { .pretendard(weight: .semibold, size: 16) }
+      static var body04: UIFont { .pretendard(weight: .bold, size: 15) }
+      static var body05: UIFont { .pretendard(weight: .regular, size: 15) }
+      static var body06: UIFont { .pretendard(weight: .bold, size: 14) }
+      static var body07: UIFont { .pretendard(weight: .bold, size: 14) }
+      static var body08: UIFont { .pretendard(weight: .medium, size: 14) }
+      static var body09: UIFont { .pretendard(weight: .medium, size: 14) }
+      
+      static var caption01: UIFont { .pretendard(weight: .bold, size: 12) }
+      static var caption02: UIFont { .pretendard(weight: .semibold, size: 12) }
+      static var caption03: UIFont { .pretendard(weight: .medium, size: 12) }
+      static var caption04: UIFont { .pretendard(weight: .regular, size: 12) }
+      static var caption05: UIFont { .pretendard(weight: .bold, size: 10) }
+      static var caption06: UIFont { .pretendard(weight: .semibold, size: 10) }
+      static var caption07: UIFont { .pretendard(weight: .medium, size: 10) }
+      static var caption08: UIFont { .pretendard(weight: .bold, size: 8) }
+      static var caption09: UIFont { .pretendard(weight: .medium, size: 8) }
 }
