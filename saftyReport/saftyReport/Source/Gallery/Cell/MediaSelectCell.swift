a602acdcd8d6483bc0a18d814494b8c9b21e4d12
OneTen19 <khy000426@naver.com>
2024-11-19 23:50:00 +0900

    Style/#10 - MediaSelectCell

--- saftyReport/saftyReport/Source/Gallery/Cell/MediaSelectCell.swift
+++ saftyReport/saftyReport/Source/Gallery/Cell/MediaSelectCell.swift
@@ -12,31 +12,47 @@ import Then
 
 class MediaSelectCell: UICollectionViewCell {
     private let photoLabel = UILabel().then {
-        $0.attributedText = .styled(text: "사진", style: .body4)
+        $0.text = "사진"
         $0.font = .systemFont(ofSize: 15, weight: .bold)
-        $0.textColor = .primary
+        $0.textColor = .primaryOrange
+        $0.textAlignment = .center
+        $0.backgroundColor = .gray1
     }
     
     private let videoLabel = UILabel().then {
-        $0.attributedText = .styled(text: "동영상", style: .body4)
+        $0.text = "동영상"
         $0.font = .systemFont(ofSize: 15, weight: .bold)
         $0.textColor = .gray1
+        $0.textAlignment = .center
+        $0.backgroundColor = .gray5
     }
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        setLayout()
+    }
     
-    private func setUI(){
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUI() {
         contentView.addSubview(photoLabel)
         contentView.addSubview(videoLabel)
     }
     
     private func setLayout() {
         photoLabel.snp.makeConstraints {
-            <#code#>
+            $0.leading.top.bottom.equalToSuperview()
+            $0.width.equalToSuperview().multipliedBy(0.5)
         }
         
         videoLabel.snp.makeConstraints {
-            
+            $0.trailing.top.bottom.equalToSuperview()
+            $0.leading.equalTo(photoLabel.snp.trailing)
+            $0.width.equalToSuperview().multipliedBy(0.5)
         }
+
     }
-    
 }
