470add562885a4e251c21ad7412a55a1c63eed58
Yurim Kim <yurim830@gmail.com>
2024-11-20 18:05:54 +0900

    Add/#24 - BaseTwoButtonAlertView 생성

---
+++ saftyReport/saftyReport/Source/BaseAlert/BaseTwoButtonAlertView.swift
@@ -0,0 +1,20 @@
+//
+//  BaseTwoButtonAlertView.swift
+//  saftyReport
+//
+//  Created by 김유림 on 11/20/24.
+//
+
+import UIKit
+
+class BaseTwoButtonAlertView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
