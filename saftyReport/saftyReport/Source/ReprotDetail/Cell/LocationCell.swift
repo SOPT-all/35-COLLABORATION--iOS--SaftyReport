4ae46b845ba5f1d45f5a4ba349ea50ed8120251f
hooni <jhlee909090@gmail.com>
2024-11-20 19:23:48 +0900

    style/#20 - 발생지역 왼쪽섹션 디자인 수정

--- saftyReport/saftyReport/Source/ReprotDetail/Cell/LocationCell.swift
+++ saftyReport/saftyReport/Source/ReprotDetail/Cell/LocationCell.swift
@@ -11,11 +11,26 @@ import SnapKit
 import Then
 
 class LocationCell: BaseCell {
-    private let textField = UITextField().then {
-        $0.backgroundColor = .systemGray6
-        $0.layer.cornerRadius = 8
-        $0.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 12, height: 0))
-        $0.leftViewMode = .always
+    private let smallLocationIcon = UIImageView().then {
+        let image = UIImage(named: "icn_location_line_black_24px")
+        $0.image = image
+        $0.contentMode = .scaleAspectFit
+    }
+    
+    private let locationLabel = UILabel().then {
+        let attributedText = NSAttributedString.styled(
+            text: "지역을 입력해주세요",
+            style: .body9
+        )
+        $0.attributedText = attributedText
+        $0.textColor = .gray
+    }
+    
+    private let locationIcon = UIImageView().then {
+        let image = UIImage(named: "btn_i_location")?.withRenderingMode(.alwaysTemplate)
+        $0.image = image
+        $0.tintColor = .gray
+        $0.contentMode = .scaleAspectFit
     }
     
     override init(frame: CGRect) {
@@ -28,19 +43,35 @@ class LocationCell: BaseCell {
     }
     
     private func setupUI() {
-        contentView.addSubview(textField)
+        contentView.addSubviews(smallLocationIcon, locationLabel, locationIcon)
         
-        textField.snp.makeConstraints {
+        smallLocationIcon.snp.makeConstraints {
             $0.top.equalTo(titleLabel.snp.bottom).offset(8)
-            $0.leading.trailing.equalToSuperview()
-            $0.height.equalTo(44)
-            $0.bottom.equalToSuperview()
+            $0.leading.equalToSuperview()
+            $0.size.equalTo(12)
+        }
+        
+        locationLabel.snp.makeConstraints {
+            $0.centerY.equalTo(smallLocationIcon)
+            $0.leading.equalTo(smallLocationIcon.snp.trailing).offset(4)
+        }
+        
+        locationIcon.snp.makeConstraints {
+            $0.centerY.equalTo(locationLabel)
+            $0.trailing.equalToSuperview()
+            $0.size.equalTo(32)
         }
     }
     
     override func configure(with item: ReportDetailItem) {
         super.configure(with: item)
-        textField.placeholder = item.placeholder
+        if let placeholder = item.placeholder {
+            let attributedText = NSAttributedString.styled(
+                text: placeholder,
+                style: .body9
+            )
+            locationLabel.attributedText = attributedText
+        }
     }
 }
 
