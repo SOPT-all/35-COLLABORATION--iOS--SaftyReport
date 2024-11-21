e64bc4323b260eec576497440369e74b34cc5eb3
OneTen19 <khy000426@naver.com>
2024-11-21 22:26:57 +0900

    Setting/#26 - add ReportResponse

--- saftyReport/saftyReport/Network/DTO/Response/ReportResponse.swift
+++ saftyReport/saftyReport/Network/DTO/Response/ReportResponse.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+// MARK: - ReportResponse
+
+struct ReportResponse: Codable {
+    let status: Int?
+    let message: String?
+    let data: ReportDataObject?
+}
+
+// MARK: - DataClass
+
+struct ReportDataObject: Codable {
+    let reportID: Int?
+    let photoList: [ReportPhotoList]?
+    let address, content, phoneNumber, category: String?
+}
+
+// MARK: - PhotoList
+
+struct ReportPhotoList: Codable {
+    let photoID: Int?
+    let photoURL: String?
+}
