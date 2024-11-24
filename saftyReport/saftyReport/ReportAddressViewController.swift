eefc6ff0dd55c43283cb3fd5d5046167d7d645c5
OneTen19 <khy000426@naver.com>
2024-11-24 16:47:59 +0900

    Style/#36 - Add ReportAddressViewController

---
+++ saftyReport/saftyReport/ReportAddressViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ReportAddressViewController.swift
+//  saftyReport
+//
+//  Created by OneTen on 11/24/24.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+class ReportAddressViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
