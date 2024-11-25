79f35fed833d3282829b71b89da1337d21347dae
hooni <jhlee909090@gmail.com>
2024-11-25 15:39:55 +0900

    add/#42- 기본 틀 완성 및 이미지 삽입

--- saftyReport/saftyReport/Source/ReportDetail/Cell/PhotoCell.swift
+++ saftyReport/saftyReport/Source/ReportDetail/Cell/PhotoCell.swift
@@ -11,98 +11,96 @@ import SnapKit
 import Then
 
 class PhotoCell: BaseCell {
-    private let photoButton = UIButton().then {
-        $0.backgroundColor = .gray3
-        $0.layer.cornerRadius = 8
-        let attributedTitle = NSAttributedString.styled(
-            text: "사진을 추가해주세요",
-            style: .body9
-        )
-        $0.setAttributedTitle(attributedTitle, for: .normal)
-    }
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setupUI()
-        setupAction()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    private func setupUI() {
-        contentView.addSubview(photoButton)
-        
-        photoButton.snp.makeConstraints {
-            $0.top.equalTo(titleLabel.snp.bottom).offset(8)
-            $0.leading.trailing.equalToSuperview()
-            $0.height.equalTo(44)
-            $0.bottom.equalToSuperview()
-        }
-    }
-    
-    private func setupAction() {
-        photoButton.addTarget(
-            self,
-            action: #selector(photoButtonTapped),
-            for: .touchUpInside
-        )
-    }
-    
-    @objc private func photoButtonTapped() {
-        guard let viewController = self.findViewController() else { return }
-        
-        let alertVC = BaseOneButtonAlertViewController()
-        
-        let contentView = UIView()
-        let messageLabel = UILabel().then {
-            $0.numberOfLines = 0
-            $0.textAlignment = .center
-            $0.attributedText = NSAttributedString.styled(
-                text: "이미지가떠야되는뎅",
-                style: .body3
-            )
-        }
-        
-        contentView.addSubview(messageLabel)
-        messageLabel.snp.makeConstraints {
-            $0.edges.equalToSuperview().inset(20)
-        }
-        
-        alertVC.setAlert(
-            "미디어 선택",
-            contentView,
-            .info
-        )
-        
-        alertVC.modalPresentationStyle = .overFullScreen
-        viewController.present(alertVC, animated: false)
-    }
-    
-    override func configure(with item: ReportDetailItem) {
-        super.configure(with: item)
-        
-        let attributedTitle = NSAttributedString.styled(
-            text: item.placeholder ?? "",
-            style: .body9
-        )
-        photoButton.setAttributedTitle(attributedTitle, for: .normal)
-    }
+   private let photoButton = UIButton().then {
+       $0.backgroundColor = .gray3
+       $0.layer.cornerRadius = 8
+       let attributedTitle = NSAttributedString.styled(
+           text: "사진을 추가해주세요",
+           style: .body9
+       )
+       $0.setAttributedTitle(attributedTitle, for: .normal)
+   }
+   
+   override init(frame: CGRect) {
+       super.init(frame: frame)
+       setupUI()
+       setupAction()
+   }
+   
+   required init?(coder: NSCoder) {
+       fatalError("init(coder:) has not been implemented")
+   }
+   
+   private func setupUI() {
+       contentView.addSubview(photoButton)
+       
+       photoButton.snp.makeConstraints {
+           $0.top.equalTo(titleLabel.snp.bottom).offset(8)
+           $0.leading.trailing.equalToSuperview()
+           $0.height.equalTo(44)
+           $0.bottom.equalToSuperview()
+       }
+   }
+   
+   private func setupAction() {
+       photoButton.addTarget(
+           self,
+           action: #selector(photoButtonTapped),
+           for: .touchUpInside
+       )
+   }
+   
+   @objc private func photoButtonTapped() {
+       guard let viewController = self.findViewController() else { return }
+       
+       let contentView = UIView()
+       let imageView = UIImageView()
+       
+       imageView.image = UIImage(named: "how_to_report_detail")
+       imageView.contentMode = .scaleAspectFit
+       
+       contentView.addSubview(imageView)
+       
+       contentView.snp.makeConstraints {
+           $0.height.equalTo(350)
+       }
+       
+       imageView.snp.makeConstraints {
+           $0.edges.top.equalToSuperview().inset(12)
+           $0.edges.leading.equalToSuperview().inset(10)
+       }
+       
+       AlertManager.presentOneButtonAlert(
+           title: "신고서 작성 가이드",
+           contentView: contentView,
+           mode: .info,
+           vc: viewController
+       )
+   }
+   
+   override func configure(with item: ReportDetailItem) {
+       super.configure(with: item)
+       
+       let attributedTitle = NSAttributedString.styled(
+           text: item.placeholder ?? "",
+           style: .body9
+       )
+       photoButton.setAttributedTitle(attributedTitle, for: .normal)
+   }
 }
 
-// UIView 익스텐션 추가
 private extension UIView {
-    func findViewController() -> UIViewController? {
-        if let nextResponder = self.next as? UIViewController {
-            return nextResponder
-        } else if let nextResponder = self.next as? UIView {
-            return nextResponder.findViewController()
-        } else {
-            return nil
-        }
-    }
+   func findViewController() -> UIViewController? {
+       if let nextResponder = self.next as? UIViewController {
+           return nextResponder
+       } else if let nextResponder = self.next as? UIView {
+           return nextResponder.findViewController()
+       } else {
+           return nil
+       }
+   }
 }
+
 #Preview {
     ReportDetailViewController()
 }
