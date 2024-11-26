9d41791d81db8dec22b775b0869c8784479bc6bb
Yurim Kim <yurim830@gmail.com>
2024-11-26 11:20:40 +0900

    [Add, Feat] #35 - TableViewCell+ 추가 및 identifier 설정, ReportCategoryNormalTableViewCell 생성

---
+++ saftyReport/saftyReport/Source/ReportCategory/ReportCategoryNormalTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ReportCategoryNormalTableViewCell.swift
+//  saftyReport
+//
+//  Created by 김유림 on 11/26/24.
+//
+
+import UIKit
+
+class ReportCategoryNormalTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
