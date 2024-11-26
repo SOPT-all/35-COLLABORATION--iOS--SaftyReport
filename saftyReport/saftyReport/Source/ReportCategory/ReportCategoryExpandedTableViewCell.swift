5e69205eb36c81f364125f37967ba1ffd3416780
Yurim Kim <yurim830@gmail.com>
2024-11-27 03:09:14 +0900

    Add/#35 - 카테고리 선택 테이블뷰 셀 생성

--- saftyReport/saftyReport/Source/ReportCategory/ReportCategoryExpandedTableViewCell.swift
+++ saftyReport/saftyReport/Source/ReportCategory/ReportCategoryExpandedTableViewCell.swift
@@ -8,10 +8,25 @@
 import UIKit
 
 class ReportCategoryExpandedTableViewCell: UITableViewCell {
+    
+    // MARK: - Properties
+    
+    private let descriptionView = UIView().then {
+        $0.backgroundColor = .gray1
+        $0.layer.cornerRadius = 10
+    }
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        contentView.backgroundColor = .orange
+        
+        contentView.addSubview(descriptionView)
+        
+        descriptionView.snp.makeConstraints {
+            $0.edges.equalToSuperview().inset(20)
+            $0.height.equalTo(200)
+        }
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
--- saftyReport/saftyReport/Source/ReportCategory/ReportCategoryNormalTableViewCell.swift
+++ saftyReport/saftyReport/Source/ReportCategory/ReportCategoryNormalTableViewCell.swift
@@ -8,7 +8,17 @@
 import UIKit
 
 class ReportCategoryNormalTableViewCell: UITableViewCell {
-
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.backgroundColor = .red
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
