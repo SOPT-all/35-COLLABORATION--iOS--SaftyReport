4553dfc083e93ae9dd405847af68c44fa49a89ca
Yurim Kim <yurim830@gmail.com>
2024-11-26 09:49:51 +0900

    Add/#35 - ReportCategory View, VC 추가

---
+++ saftyReport/saftyReport/Source/ReportCategory/ReportCategoryView.swift
@@ -0,0 +1,20 @@
+//
+//  ReportCategoryView.swift
+//  saftyReport
+//
+//  Created by 김유림 on 11/26/24.
+//
+
+import UIKit
+
+class ReportCategoryView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
