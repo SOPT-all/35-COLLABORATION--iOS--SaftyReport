43b05b8fb5158c93a71ee72c4797c801e6b78918
Yurim Kim <yurim830@gmail.com>
2024-11-27 03:10:25 +0900

    Feat/#35 - 테이블뷰 snapshot 기본 구조 세팅

--- saftyReport/saftyReport/Source/ReportCategory/ReportCategoryViewController.swift
+++ saftyReport/saftyReport/Source/ReportCategory/ReportCategoryViewController.swift
@@ -8,22 +8,85 @@
 import UIKit
 
 class ReportCategoryViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    // MARK: - Properties
+    
+    private let reportCategoryView = ReportCategoryView()
+    private var dataSource: UITableViewDiffableDataSource<ReportCategory.Section, ReportCategory.Item>! = nil
+    
+    
+    // MARK: - Methods
+    
+    override func loadView() {
+        view = reportCategoryView
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setTableView()
+        setDataSource()
+        applyInitialSnapshots()
     }
-    */
+    
+    private func setTableView() {
+        reportCategoryView.tableView.dataSource = self
+        
+        reportCategoryView.tableView.register(
+            ReportCategoryNormalTableViewCell.self,
+            forCellReuseIdentifier: ReportCategoryNormalTableViewCell.identifier
+        )
+        
+        reportCategoryView.tableView.register(
+            ReportCategoryExpandedTableViewCell.self,
+            forCellReuseIdentifier: ReportCategoryExpandedTableViewCell.identifier
+        )
+    }
+    
+    private func setDataSource() {
+//        reportCategoryView.tableView.dataSource = UITableViewDiffableDataSource
+        dataSource = UITableViewDiffableDataSource
+            <ReportCategory.Section, ReportCategory.Item>(tableView:
+                                                            reportCategoryView.tableView) {
+                [weak self] (tableView: UITableView,
+                             indexPath: IndexPath,
+                             item: ReportCategory.Item) -> UITableViewCell? in
+                guard let self = self else { return nil }
+                
+                let cell = item.toggleState == .normal
+                ? reportCategoryView.tableView.dequeueReusableCell(
+                    withIdentifier: ReportCategoryNormalTableViewCell.identifier,
+                    for: indexPath)
+                : reportCategoryView.tableView.dequeueReusableCell(withIdentifier: ReportCategoryExpandedTableViewCell.identifier, for: indexPath)
+                
+                return cell
+            }
+    }
+    
+    private func applyInitialSnapshots() {
+        var snapshot = NSDiffableDataSourceSnapshot<ReportCategory.Section, ReportCategory.Item>()
+        snapshot.appendSections([.safety, .parking, .traffic, .environment])
+        
+        snapshot.appendItems(ReportCategory.Dummy.safety, toSection: .safety)
+        snapshot.appendItems(ReportCategory.Dummy.parking, toSection: .parking)
+        snapshot.appendItems(ReportCategory.Dummy.traffic, toSection: .traffic)
+        snapshot.appendItems(ReportCategory.Dummy.environment, toSection: .environment)
+        
+        dataSource.apply(snapshot, animatingDifferences: false)
+    }
+}
 
+extension ReportCategoryViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        print("sections: \(ReportCategory.Section.allCases.count)개")
+        return ReportCategory.Section.allCases.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("rows: \(dataSource.snapshot().itemIdentifiers(inSection: ReportCategory.Section.allCases[section]).count)개")
+        return dataSource.snapshot().itemIdentifiers(inSection: ReportCategory.Section.allCases[section]).count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        fatalError("This method shouldn't be called as the data source. It is provided by the diffable data source.")
+    }
 }
