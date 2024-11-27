1a26989e9ec5fe1c01e411899b6207ad7f4e67dd
Yurim Kim <yurim830@gmail.com>
2024-11-28 02:03:55 +0900

    Style/#52 - 더미데이터 수정

--- saftyReport/saftyReport/Source/ReportCategory/ReportCategory.swift
+++ saftyReport/saftyReport/Source/ReportCategory/ReportCategory.swift
@@ -30,8 +30,8 @@ extension ReportCategory {
         
         static let parking = [ReportCategory.Item(section: .safety, name: "불법 주정차", description: "\"소화전 \\r\\n 교차로 모퉁이 \\r\\n 버스 정류소 \\r\\n 횡단보도 \\r\\n 어린이 보호구역 \\r\\n 인도 \\r\\n 장애인 전용구역 \\r\\n 소방차 전용구역 \\r\\n 친환경차 전용구역 \\r\\n 기타\"", isExpanded: false)]
         
-        static let traffic = [ReportCategory.Item(section: .traffic, name: "자동차/교통위반", description: "\"교통위반(고속도로 포함) \\r\\n \"이륜차 위반\" \\r\\n 버스전용차로 위반(고속도로 제회) \\r\\n 불법동화, 반사판(지) 가림, 손상 \\r\\n 불법 튜닝, 해체, 조작 \\r\\n 기타 자동차 안전기준 위반", isExpanded: true)]
+        static let traffic = [ReportCategory.Item(section: .traffic, name: "자동차/교통위반", description: "\"교통위반(고속도로 포함) \\r\\n \"이륜차 위반\" \\r\\n 버스전용차로 위반(고속도로 제회) \\r\\n 불법동화, 반사판(지) 가림, 손상 \\r\\n 불법 튜닝, 해체, 조작 \\r\\n 기타 자동차 안전기준 위반", isExpanded: false)]
         
-        static let environment = [ReportCategory.Item(section: .environment, name: "생활불편", description: "\"불법광고물 \\r\\n 자전거/이륜차 방치 및 불편 \\r\\n 자전거/이륜차 방치 및 불편 \\r\\n쓰레기, 폐기물 \\r\\n해양 쓰레기 \\r\\n 기타 생활불편\"", isExpanded: true)]
+        static let environment = [ReportCategory.Item(section: .environment, name: "생활불편", description: "\"불법광고물 \\r\\n 자전거/이륜차 방치 및 불편 \\r\\n 자전거/이륜차 방치 및 불편 \\r\\n쓰레기, 폐기물 \\r\\n해양 쓰레기 \\r\\n 기타 생활불편\"", isExpanded: false)]
     }
 }
