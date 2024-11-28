e1c1a61b164b7c1ffc375e654934fa476f8652b9
Yurim Kim <yurim830@gmail.com>
2024-11-29 04:40:41 +0900

    Chore/#59 - 잘못 수정된 부분 되돌리기

--- saftyReport/saftyReport/Resource/Custom/TabBar/CustomTabBarItem.swift
+++ saftyReport/saftyReport/Resource/Custom/TabBar/CustomTabBarItem.swift
@@ -16,7 +16,7 @@ enum CustomTabBarItem: CaseIterable {
         case .home: return MainViewController()
         case .report: return ViewController()
         case .prevent: return ViewController()
-        case .news: return ReportViewController()
+        case .news: return ViewController()
         case .myPage: return ViewController()
         }
     }
