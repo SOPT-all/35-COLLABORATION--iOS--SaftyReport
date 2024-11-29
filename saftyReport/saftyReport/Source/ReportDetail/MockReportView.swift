89413c8b3736887b7b21686b54332fdac5fd88e0
hee <heheeun13@gmail.com>
2024-11-29 18:03:58 +0900

    Style/#70 StackView로 category button 작업

--- saftyReport/saftyReport/Source/ReportDetail/MockReportView.swift
+++ saftyReport/saftyReport/Source/ReportDetail/MockReportView.swift
@@ -11,8 +11,25 @@ import SnapKit
 import Then
 
 class MockReportView: UIView {
+    
+    let topOptions = [
+        "소화전",
+        "교차로 모퉁이",
+        "버스 정류소",
+        "황단보도",
+        "어린이 보호구역",
+        "인도",
+        "기타"
+    ]
+    
+    let bottomOptions = [
+        "장애인 전용구역",
+        "소방차 전용구역",
+        "친환경차 충전구역"
+    ]
+    
     let mainButton = UIButton().then {
-        $0.backgroundColor = .systemGray6
+        $0.backgroundColor = .gray3
         $0.contentHorizontalAlignment = .left
         $0.layer.cornerRadius = 8
         $0.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
@@ -29,11 +46,12 @@ class MockReportView: UIView {
         $0.configuration = configuration
     }
     
-    private let optionsContainer = UIView().then {
+    let optionsContainer = UIView().then {
         $0.backgroundColor = .white
-        $0.layer.cornerRadius = 8
+        $0.layer.cornerRadius = 15
         $0.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
-        CustomShadow.shared.applyShadow(to: $0.layer, width: 0, height: 4)
+        $0.backgroundColor = .gray3
+        CustomShadow.shared.applyShadow(to: $0.layer, width: 0, height: -4)
         $0.isHidden = true
     }
     
@@ -41,10 +59,29 @@ class MockReportView: UIView {
         $0.image = UIImage.icnArrowDownLineBlack24Px
         $0.tintColor = .darkGray
     }
-
+    
     private let optionsStackView = UIStackView().then {
         $0.axis = .vertical
         $0.spacing = 12
+        $0.distribution = .fillProportionally
+    }
+    
+    private let dividerView = UIView().then {
+        $0.backgroundColor = .gray13Opacity10
+    }
+    
+    private lazy var topStackView = UIStackView().then {
+        $0.backgroundColor = .clear
+        $0.axis = .vertical
+        $0.spacing = 10
+        $0.distribution = .fillEqually
+    }
+    
+    private lazy var bottomStackView = UIStackView().then {
+        $0.backgroundColor = .clear
+        $0.axis = .vertical
+        $0.spacing = 10
+        $0.distribution = .fillEqually
     }
     
     var isExpanded: Bool = false {
@@ -83,9 +120,11 @@ class MockReportView: UIView {
             if let window = self.window {
                 window.addSubview(self.optionsContainer)
                 self.optionsContainer.addSubview(self.optionsStackView)
-                
                 self.optionsStackView.snp.makeConstraints {
                     $0.edges.equalToSuperview().inset(16)
+                }
+                self.dividerView.snp.makeConstraints {
+                    $0.height.equalTo(2)
                 }
             }
         }
@@ -97,24 +136,92 @@ class MockReportView: UIView {
         mainButton.layer.cornerRadius = 15
         mainButton.layer.maskedCorners = [
             .layerMinXMaxYCorner,
-            .layerMaxXMaxYCorner 
+            .layerMaxXMaxYCorner
         ]
     }
     
     private func setupOptions() {
-        let options = ["소화전", "교차로 모퉁이", "버스 정류소", "황단보도",
-                      "어린이 보호구역", "인도", "기타", "장애인 전용구역", "친환경차 충전구역"]
+        var currentRowStackView: UIStackView?
+        var currentButtonCount = 0
         
-        options.forEach { option in
-            let button = UIButton(type: .system).then {
-                $0.setTitle(option, for: .normal)
-                $0.setTitleColor(.black, for: .normal)
-                $0.contentHorizontalAlignment = .left
-                $0.titleLabel?.font = .systemFont(ofSize: 14)
-                $0.addTarget(self, action: #selector(optionSelected(_:)), for: .touchUpInside)
+        topOptions.forEach { option in
+            // "기타"는 단독으로 표시
+            
+            if topOptions.count%2 == 0 && option == topOptions.last {
+                let button = setButton(buttonTitle: option)
+                let singleButtonStack = UIStackView().then {
+                    $0.axis = .horizontal
+                    $0.spacing = 10
+                    $0.alignment = .fill
+                }
+                
+                singleButtonStack.addArrangedSubview(button)
+                topStackView.addArrangedSubview(singleButtonStack)
+                return
+            } else {
+                // 새로운 Row StackView가 필요한 경우 생성
+                if currentRowStackView == nil || currentButtonCount == 2 {
+                    currentRowStackView = UIStackView().then {
+                        $0.axis = .horizontal
+                        $0.spacing = 10
+                        $0.alignment = .fill
+                        $0.distribution = .fillEqually
+                    }
+                    topStackView.addArrangedSubview(currentRowStackView!)
+                    
+                    print(currentButtonCount, topStackView.arrangedSubviews)
+                    currentButtonCount = 0
+                }
+                
+                // 버튼 생성 및 Row StackView에 추가
+                if let currentRow = currentRowStackView {
+                    let button = setButton(buttonTitle: option)
+                    currentRow.addArrangedSubview(button)
+                    currentButtonCount += 1
+                }
             }
-            optionsStackView.addArrangedSubview(button)
         }
+        
+        var bottomCurrentRowStackView: UIStackView?
+        var bottomCurrentButtonCount = 0
+        bottomOptions.forEach { option in
+            if bottomCurrentRowStackView == nil || bottomCurrentButtonCount == 2 {
+                bottomCurrentRowStackView = UIStackView().then {
+                    $0.axis = .horizontal
+                    $0.spacing = 10
+                    $0.alignment = .fill
+                    $0.distribution = .fillEqually
+                }
+                bottomStackView.addArrangedSubview(bottomCurrentRowStackView!)
+                
+                print(bottomCurrentButtonCount, bottomStackView.arrangedSubviews)
+                currentButtonCount = 0
+            }
+            
+            // 버튼 생성 및 Row StackView에 추가
+            if let currentRow = bottomCurrentRowStackView {
+                let button = setButton(buttonTitle: option)
+                currentRow.addArrangedSubview(button)
+                bottomCurrentButtonCount += 1
+            }
+        }
+        
+        optionsStackView.addArrangedSubviews(topStackView, dividerView, bottomStackView)
+    }
+    
+    private func setButton(buttonTitle: String) -> UIButton {
+        let button = UIButton().then {
+            let attributedText = NSAttributedString.styled(text: buttonTitle, style: .body6)
+            $0.setAttributedTitle(attributedText, for: .normal)
+            $0.setTitle(buttonTitle, for: .normal)
+            $0.setTitleColor(.gray13, for: .normal)
+            $0.contentHorizontalAlignment = .center
+            $0.titleLabel?.font = .systemFont(ofSize: 14)
+            $0.backgroundColor = .gray1
+            $0.layer.cornerRadius = 10
+            $0.addTarget(self, action: #selector(optionSelected(_:)), for: .touchUpInside)
+        }
+        return button
     }
     
     @objc private func optionSelected(_ sender: UIButton) {
@@ -126,6 +233,7 @@ class MockReportView: UIView {
             isExpanded = false
         }
     }
+    
     private func updateUI() {
         guard let window = self.window else { return }
         
@@ -136,7 +244,7 @@ class MockReportView: UIView {
                 x: buttonFrame.minX,
                 y: buttonFrame.maxY + 8,
                 width: buttonFrame.width,
-                height: 400
+                height: 400//window.frame.height * 0.4
             )
             optionsContainer.isHidden = false
         }
